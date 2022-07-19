a7f0558ad8713a03e5d1b2102cc621404ee00881
Yifan <datasciencewyfan@gmail.com>
2022-07-19 15:11:35 -0700

    commit tree 5f47826168c55633ed9fd238d655f2a6a75c0840

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,23 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    puts params[:name]
+    if params[:name]
+      if Student.find_by(first_name: params[:name])
+        students = Student.where("first_name = '#{params[:name]}'")
+      elsif Student.find_by(last_name: params[:name])
+        students = Student.where("last_name = '#{params[:name]}'")
+      end
+      render json: students
+    else
+      students = Student.all
+      render json: students
+    end
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
